a0d5f551b82b9d7baea92e5bea179eac86f326ab
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2021-01-30 21:52:08 +0100

    Add new driver, remove run_video

---
+++ python/run_simulation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# File       : run_simulation.py
+# Created    : Sat Jan 30 2021 09:13:51 PM (+0100)
+# Description: Gray-Scott driver.  Use the --help argument for all options
+# Copyright 2021 ETH Zurich. All Rights Reserved.
+import argparse
+
+# the file gray_scott.py must be in the PYTHONPATH or in the current directory
+from gray_scott import GrayScott
+
+def parse_args():
+    """
+    Driver arguments.  These are passed on to the GrayScott class to setup the 
+    problem.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-F', '--feed_rate', default=0.04, type=float, help='Feed rate F')
+    parser.add_argument('-k', '--death_rate', default=0.06, type=float, help='Death rate kappa')
+    parser.add_argument('-T', '--end_time', default=3500, type=float, help='Final time')
+    parser.add_argument('--demo', action='store_true', help='Run a demo (https://www.chebfun.org/examples/pde/GrayScott.html)')
+    parser.add_argument('--movie', action='store_true', help='Create a movie')
+    parser.add_argument('--outdir', default='.', type=str, help='Output directory')
+    return parser.parse_known_args()
+
+def demo():
+    """
+    Reproduces the example at https://www.chebfun.org/examples/pde/GrayScott.html
+    Pass the --demo option to the driver to run this demo.
+    """
+
+    # 1. Rolls
+    rolls = GrayScott(F=0.04, kappa=0.06, movie=True, outdir="demo_rolls")
+    rolls.integrate(0, 3500)
+
+    # # 2. Spots
+    # spots = GrayScott(F=0.025, kappa=0.06, movie=True, outdir="demo_spots")
+    # spots.integrate(0, 3500)
+
+
+def main():
+    args, _ = parse_args()
+
+    if args.demo:
+        demo()
+        return
+
+    gs = GrayScott(F=args.feed_rate, kappa=args.death_rate, movie=args.movie, outdir=args.outdir)
+    gs.integrate(0, args.end_time)
+
+
+if __name__ == "__main__":
+    main()
